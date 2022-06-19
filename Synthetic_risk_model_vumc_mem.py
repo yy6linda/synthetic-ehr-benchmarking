7af3838fa0c857bb655b4278b650dacbe55483d8
Zhiyu Wan <zhywan@gmail.com>
2022-06-19 17:55:49 -0500

    Update Synthetic_risk_model_vumc_mem.py

--- Synthetic_risk_model_vumc_mem.py
+++ Synthetic_risk_model_vumc_mem.py
@@ -1,8 +1,25 @@
 # 03/15/2022 edited by Zhiyu Wan
+# 05/14/2022 add normalization
 import numpy as np
 import os.path
 import time
+import sys
 
+'''
+Usage: synthetic_risk_model_vumc_mem.py [model] [exp_id] [theta] [train_filename] [test_filename] [prefix_syn] [infix_syn] [output_directory]
+
+Example: synthetic_risk_model_vumc_mem.py iwae 1 32 train_vumc test_vumc syn_ _vumc _ Results_Synthetic_VUMC/
+
+1. [model]: name of data generation model. Selected from ['iwae', 'medgan', 'medbgan', 'emrwgan', 'medwgan', 'dpgan', 'real']. Default: 'iwae'.
+2. [exp_id]: No. of the experiment. Selected from ['1', '2', '3']. Default: '1'.
+3. [theta]: the threshold for the euclidean distance between two records. Default: '32'. Try: '64'.
+4. [train_filename]: the filename of the training file. Default: 'train_vumc'.
+5. [test_filename]: the filename of the test file. Default: 'test_vumc'.
+6. [prefix_syn]: the prefix of the synthetic filename. Default: 'syn_'.
+7. [suffix_syn]: the suffix of the synthetic filename. Default: '_vumc'.
+8. [infix_syn]: the suffix of the synthetic filename in the middle of [model_name] and [exp_id]. Default: '_'.
+9. [output_directory]: output directory. Default: 'Results_Synthetic_VUMC/'.
+'''
 
 def get_entropy(column):
     (hist, bin_edges) = np.histogram(column, bins=np.arange(min(column), max(column) + 2))
@@ -11,41 +28,15 @@ def get_entropy(column):
     entropy = -np.sum(pk * np.log(pk))
     return entropy
 
+
 def find_replicant(real, fake):
     a = np.sum(fake ** 2, axis=1).reshape(fake.shape[0],1) + np.sum(real.T ** 2,axis=0)
     b = np.dot(fake, real.T) * 2
     distance_matrix = a - b
     return np.min(distance_matrix, axis=0)
 
+
 def each_group(model):
-    train_patient_filename = 'train_raw_14349'
-    train = np.genfromtxt('data/' + train_patient_filename + '.csv', delimiter=',', skip_header=1)
-    test_patient_filename = 'test_raw'
-    test = np.genfromtxt('data/' + test_patient_filename + '.csv', delimiter=',', skip_header=1)
-    if model == 'real':
-        fake = train
-    else:
-        fake_filename = 'round_syn_' + model + '_' + Exp_ID
-        fake = np.genfromtxt('data/' + fake_filename + '.csv', delimiter=',', skip_header=1)
-
-    # compute weights
-    #entropy = []
-    #n_attr = np.shape(train)[1]  # number of attributes
-    #for j in range(n_attr):
-    #    entropy.append(get_entropy(train[:, j]))
-    #weight = np.asarray(entropy) / sum(entropy)
-
-    # normalization
-    #
-    #normal_range = np.maximum(np.amax(fake, axis=0) - np.amin(fake, axis=0), 1)
-    #train = train / normal_range
-    #test = test / normal_range
-    #fake = fake / normal_range
-    #normal_weight = weight / normal_range
-    #normal_weight_mat = normal_weight.reshape(-1, 1)
-
-    n_train = np.shape(train)[0]
-    n_test = np.shape(test)[0]
     distance_train = np.zeros(n_train)
     distance_test = np.zeros(n_test)
     if model != 'real':
@@ -57,31 +48,85 @@ def each_group(model):
     for i in range(int(steps)):
         distance_test[i * batchsize:(i + 1) * batchsize] = find_replicant(test[i * batchsize:(i + 1) * batchsize], fake)
 
-    n_fp = np.floor(beta * n_test).astype(int)  # tolerable false positive counts
-    sorted_distance_test = np.sort(distance_test, axis=None)
-    theta = sorted_distance_test[n_fp]  # threshold
     n_tp = np.sum(distance_train <= theta)  # true positive counts
+    n_fn = n_train - n_tp
     n_fp = np.sum(distance_test <= theta)  # false positive counts
-    precision = n_tp / (n_tp+n_fp)
-    recall = n_tp / n_train  # true positive rate
-    f1 = 2 * precision * recall / (precision + recall)  # F1 score
+    f1 = n_tp / (n_tp + (n_fp + n_fn) / 2)  # F1 score
     return f1
 
 
 if __name__ == '__main__':
-    batchsize = 1000
-    Exp_name = "Mem_Risk_"
-    Exp_ID = "3"
-    Result_folder = "Results_Synthetic_Mem/"
-    beta = 0.05
-    if not os.path.exists(Result_folder):
-        os.mkdir(Result_folder)
-    print(Result_folder)
-    results = []
-    start1 = time.time()
-    for m in ['iwae', 'medgan', 'medbgan','emrwgan', 'medwgan', 'dpgan', 'real']:
-        print("model=" + m)
-        results.append(each_group(m))
-    elapsed1 = (time.time() - start1)
-    print("Time used: " + str(elapsed1) + " seconds.\n")
-    np.savetxt(Result_folder + Exp_name + "Ex" + Exp_ID + ".csv", np.array(results), delimiter=",")
+    # Default configuration
+    for model in ['iwae', 'medgan', 'medbgan', 'medwgan', 'emrwgan', 'dpgan', 'real']:
+        #model = 'medgan'
+        exp_id = "1"
+        theta = 5
+        train_patient_filename = 'train_raw_correct'
+        test_patient_filename = 'test_raw_correct'
+        prefix_syn = 'correct_round_syn_'
+        suffix_syn = ''
+        infix_syn = '_'
+        Result_folder = "Results_Synthetic_VUMC_V3/"
+        if not os.path.exists(Result_folder):
+            os.mkdir(Result_folder)
+        batchsize = 1000
+        exp_name = "Mem_Risk"
+
+        start1 = time.time()
+        # Enable the input of parameters
+        if len(sys.argv) >= 2:
+            model = sys.argv[1]
+        if len(sys.argv) >= 3:
+            exp_id = sys.argv[2]
+        if len(sys.argv) >= 4:
+            theta = float(sys.argv[3])
+        if len(sys.argv) >= 5:
+            train_patient_filename = sys.argv[4]
+        if len(sys.argv) >= 6:
+            test_patient_filename = sys.argv[5]
+        if len(sys.argv) >= 7:
+            prefix_syn = sys.argv[6]
+        if len(sys.argv) >= 8:
+            suffix_syn = sys.argv[7]
+        if len(sys.argv) >= 9:
+            infix_syn = sys.argv[8]
+        if len(sys.argv) >= 10:
+            Result_folder = sys.argv[9]
+        print("output_directory: " + Result_folder)
+        print("train_filename: " + train_patient_filename)
+        print("test_filename: " + test_patient_filename)
+        print("syn_filename: " + prefix_syn + model + infix_syn + exp_id + suffix_syn)
+        print("theta: " + str(theta))
+        # load datasets
+        train = np.load('data/' + train_patient_filename + '.npy')
+        test = np.load('data/' + test_patient_filename + '.npy')
+        n_train = np.shape(train)[0]
+        n_test = np.shape(test)[0]
+        if model == 'real':
+            fake = train.copy()
+        else:
+            fake_filename = prefix_syn + model + infix_syn + exp_id + suffix_syn
+            fake = np.load('data/' + fake_filename + '.npy')
+        elapsed1 = (time.time() - start1)
+        start2 = time.time()
+        # normalization
+        [n_row, n_col] = fake.shape
+        for j in range(n_col-8, n_col):
+            normal_max = np.amax(fake[:, j])
+            normal_min = np.amin(fake[:, j])
+            normal_range = normal_max - normal_min
+            fake[:, j] = (fake[:, j] - normal_min) / normal_range
+            train[:, j] = (train[:, j] - normal_min) / normal_range
+            test[:, j] = (test[:, j] - normal_min) / normal_range
+
+        result = each_group(model)
+        elapsed2 = (time.time() - start2)
+        print("Risk: " + str(result) + ".")
+        print("Time used: " + str(elapsed1 + elapsed2) + " seconds.")
+        print("Loading time used: " + str(elapsed1) + " seconds.")
+        print("Computing time used: " + str(elapsed2) + " seconds.")
+        with open(Result_folder + exp_name + "_" + model + "_" + exp_id + "_theta" + str(theta) + ".txt", 'w') as f:
+            f.write(str(result) + "\n")
+            f.write("Time used: " + str(elapsed1 + elapsed2) + " seconds.\n")
+            f.write("Loading time used: " + str(elapsed1) + " seconds.\n")
+            f.write("Computing time used: " + str(elapsed2) + " seconds.\n")
