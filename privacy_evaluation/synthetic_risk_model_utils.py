f0522174d904a3d22b4f8d3146686032f9ba9285
Zhiyu Wan <zhywan@gmail.com>
2022-08-11 04:29:14 -0500

    Update synthetic_risk_model_utils.py

--- privacy_evaluation/synthetic_risk_model_utils.py
+++ privacy_evaluation/synthetic_risk_model_utils.py
@@ -32,6 +32,38 @@ def prepare_data(file):
                 fake_data2[i, j] = fake_data[i, j + 4].astype(int)
     return fake_data2
 
+
+def prepare_data_vumc(file):
+    fake_data = np.load(file)
+    print("Loading data from: " + file)
+    [n_row, n_col] = fake_data.shape
+    fake_data2 = np.zeros((n_row, n_col - 4))
+    for i in range(n_row):
+        if i % 100 == 0:
+            print("patient#: " + str(i))
+        for j in range(n_col - 4):
+            if j == 0:
+                fake_data2[i, j] = np.floor(fake_data[i, 6] + 0.5).astype(int)
+            elif j == 1:
+                fake_data2[i, j] = np.floor(fake_data[i, n_col - 1]).astype(int)
+            elif j == 2:
+                if fake_data[i, 0] == 1:
+                    fake_data2[i, j] = 1
+                elif fake_data[i, 1] == 1:
+                    fake_data2[i, j] = 2
+                elif fake_data[i, 2] == 1:
+                    fake_data2[i, j] = 3
+                elif fake_data[i, 3] == 1:
+                    fake_data2[i, j] = 4
+            elif j == 3:
+                fake_data2[i, j] = np.floor(fake_data[i, 5] + 0.5).astype(int)
+            elif j >= n_col - 11:
+                fake_data2[i, j] = fake_data[i, j + 3]
+            else:
+                fake_data2[i, j] = np.floor(fake_data[i, j + 3] + 0.5).astype(int)
+    return fake_data2
+
+
 def reorder_data(file):
     fake_data = np.load(file)
     print("Loading data from: " + file)
